c0fca0680a9bdf75f0077619b26424e35e1b8854
Arslaan Ahmad <arslaanactive@gmail.com>
2025-11-30 23:35:11 +0530

    New Website Update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,15 +4,22 @@ import { useRef } from "react";
 
 // Portfolio items array
 const items = [
-  {
+    {
     id: 1,
+    title: "Next Hire Recruitment",
+    img: "/nexthire.png",
+    desc: "This is a Full responsive UK Based Client's website for Job Posting and Hiring for there B2B Business. Along with there Admin dashboard for controlling full website with No-code",
+    link: "https://nexthirerecruitment.co.uk/",
+  },
+  {
+    id: 2,
     title: "CME Trading Services",
     img: "/CME.png",
     desc: "A responsive website for a Qatar-based industry featuring modern design, smooth animations, and seamless navigation, ensuring an engaging user experience while efficiently managing order-related files and data.",
     link: "https://cme.qa/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Welfare Action for Incarcerated (WAFI)",
     img: "/wafi.png",
     desc: "A project aimed at supporting incarcerated individuals through education, mental health, and skill development programs, promoting reintegration into society.",
@@ -20,14 +27,14 @@ const items = [
   },
 
   {
-    id: 3,
+    id: 4,
     title: "BiDepartment Database Management",
     img: "/Connective.png",
     desc: "A centralized dashboard for managing and organizing order-related Files, Activity Tracker, and Data, ensuring efficient and easy access for each individual order.",
     link: "https://dashboard.enggenv.com/connectivityDash/#/connectivity",
   },
   {
-    id: 4,
+    id: 5,
     title: "Noor al-fikr",
     img: "/noor.png",
     desc: "A responsive website for a Saudi-based industry featuring modern design, smooth animations, and seamless navigation, ensuring an engaging user experience while efficiently managing order-related files and data.",
@@ -35,14 +42,14 @@ const items = [
   },
 
   {
-    id: 5,
+    id: 6,
     title: "Calibrix (Certificate Generation System)",
     img: "/calibrix.png",
     desc: "Clibrix offers the ability to create custom certificates tailored to individual user requirements, providing a flexible solution for generating certificates with personalized details and designs.",
     link: "https://dashboard.enggenv.com/calib_cert/#/login",
   },
   {
-    id: 6,
+    id: 7,
     title: "Ontato",
     img: "/ontato.png",
     desc: "A responsive website for a Australia Based industry featuring modern design, smooth animations, and seamless navigation, ensuring an engaging user experience while efficiently managing order-related files and data.",
@@ -50,35 +57,35 @@ const items = [
   },
 
   {
-    id: 7,
+    id: 8,
     title: "Employee Management Dashboard (In Progress)",
     img: "https://miro.medium.com/v2/resize:fit:1400/1*3oAf0XMPxo2OoIeIK0FFwg.png",
     desc: "A comprehensive platform designed for efficient management of employee data and processes, offering tools for tracking performance, attendance, and payroll.",
     // link: "https://github.com/Arslaan027",
   },
   {
-    id: 8,
+    id: 9,
     title: "Sigma Solution WebApp  (In Progress)",
     img: "https://xdfile.com/wp-content/uploads/2020/04/Software-Company-XD-Website-Template-758x427.jpg",
     desc: "A multi-service platform offering a wide range of business solutions, from registrations to digital marketing, designed for global enterprises.",
     link: "https://sigmasolution.onrender.com/",
   },
   {
-    id: 9,
+    id: 10,
     title: "Video Advertisement",
     img: "/VideoAdd.mp4",
     desc: "A creative video advertisement designed to promote a restaurant, focusing on its food offerings and running targeted campaigns across social media platforms.",
     // link: "#Contact",
   },
   {
-    id: 10,
+    id: 11,
     title: "Meme Creation",
     img: "/meme.mp4",
     desc: "A fun and engaging meme creation project aimed at leveraging viral content to boost brand visibility and connect with a younger audience through humor on social media.",
     // link: "#Contact",
   },
   {
-    id: 11,
+    id: 12,
     title: "WordPress Website (DesktopView Only)",
     img: "/wordpress.png",
     desc: "A professional WordPress website designed to showcase business services and products, optimized for user engagement and easy content management.",
